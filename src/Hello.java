20af1ae9d1d0812a22ca085b690ab8f686ff9163
Avinash Shinde <avinashshide5495@gmail.com>
2022-08-22 19:00:51 +0530

    2nd commit

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Avinash, This is your first project");
-
+        System.out.println("Hello Avinash, This is your second commit");
 	}
 
 }
